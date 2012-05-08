4ff5431ef8151d0b09e4ee47f9345bf123d60b97
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2012-05-08 00:59:46 -0300

    Mais DAOs

--- src/business/spec/IItemFaturaTelefonica.java
+++ src/business/spec/IItemFaturaTelefonica.java
@@ -13,11 +13,11 @@ public interface IItemFaturaTelefonica {
 
 	void create(ItemFaturaTelefonicaVO vo) throws BusinessException;
 
-	void delete(int mes, int ano, Calendar dataHora) throws BusinessException;
+	void delete(String numero, Calendar dataHora) throws BusinessException;
 
 	void update(ItemFaturaTelefonicaVO vo) throws BusinessException;
 
-	ItemFaturaTelefonicaVO getItemFaturaTelefonica(int mes, int ano, Calendar dataHora) throws BusinessException;
+	ItemFaturaTelefonicaVO getItemFaturaTelefonica(String numero, Calendar dataHora) throws BusinessException;
 
 	List getAll() throws BusinessException;
 
